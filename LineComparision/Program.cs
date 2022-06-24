49df92475e6fc7afb1aff4b2119db9180ebf2000
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-24 10:25:23 +0530

    [vijayalakshmi] check length of thetwo lines

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 LineComparision.Line linecomparison = new LineComparision.Line();
-double length = linecomparison.getLength(4, 5, 8, 6);
-Console.WriteLine("length of line" + length);
+double length1 = linecomparison.getLength(4, 6, 8, 6);
+double length2 = linecomparison.getLength(3, 7, 9, 11);
+Console.WriteLine("length of line 1 " + length1);
+Console.WriteLine("length of line 2 " + length2);
