db36755941c0d65ef10dc1e4bcb78f05717aa4ba
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-24 10:22:50 +0530

    [vijayalakshmi] check length of the line

---
+++ LineComparision/Line.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    internal class Line
+    {
+        public double getLength(double x1, double y1, double x2, double y2)
+        {
+            double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return result;
+        }
+    }
+}
---
+++ LineComparision/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+LineComparision.Line linecomparison = new LineComparision.Line();
+double length = linecomparison.getLength(4, 5, 8, 6);
+Console.WriteLine("length of line" + length);
